2db7956d47ea429ffd1bd69beb4ee119eb60c52a
vlmaslennikov <v.maslennikov.q@gmail.com>
2021-06-16 17:03:56 +0300

    add keyboard navigation to directive

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -7,16 +7,19 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class ColorPickerComponent   {
   palette:string[]=[
-    'red','green','blue','black','grey','orange','yellow','pink','white','violet'
+    'red','green','blue','black','grey','orange','yellow','pink','white','violet','indigo','brown'
   ];
   selectedColor!:string;
   currentColor!:string;
-  
+  toggle=true;
   @ViewChild('paletteDropdown') paletteDropdown!:ElementRef
-
 
   selectColor(){
 
   }
- 
+  addFocus(eventTarget:any){
+    // Array.from(eventTarget.parentElement.cells).map((el:any)=>el.classList.remove('selected'));
+    // eventTarget.focus();
+    // eventTarget.classList.add('selected');
+  }
 }
--- src/app/directives/picker.directive.ts
+++ src/app/directives/picker.directive.ts
@@ -1,10 +1,87 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appPicker]'
 })
 export class PickerDirective {
 
-  constructor() { }
+
+  // @Output() setValue: EventEmitter<any> = new EventEmitter<string>();
+
+  @HostListener('keydown.arrowdown', ['$event.target']) arrowDown(
+    event: EventTarget
+  ):void {
+    let eventTarget = event as any;
+    eventTarget.classList.remove('selected');
+    if (eventTarget.cellIndex >=9) {////
+      // this.setValue.emit(eventTarget.nextSibling.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex-9].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex-9].focus();
+    } else {
+      // this.setValue.emit(eventTarget.parentElement.firstElementChild.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex+3].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex+3].focus();
+    }
+  }
+
+
+  @HostListener('keydown.arrowup', ['$event.target']) arrowUp(event: EventTarget):void {
+    let eventTarget = event as any;
+    eventTarget.classList.remove('selected');
+    if (eventTarget.cellIndex <= 2) {
+      // this.setValue.emit(eventTarget.previousSibling.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex+9].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex+9].focus();
+    } else {
+      // this.setValue.emit(eventTarget.parentElement.lastElementChild.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex-3].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex-3].focus();
+    }
+  }
+
+  @HostListener('keydown.arrowleft', ['$event.target']) arrowLeft(
+    event: EventTarget
+  ):void {
+    let eventTarget = event as any;
+    eventTarget.classList.remove('selected');
+    if (eventTarget.cellIndex === 0) {
+      // this.setValue.emit(eventTarget.previousSibling.value);
+      eventTarget.cellIndex[eventTarget.parentElement.cells.length-1].classList.add('selected');
+      eventTarget.cellIndex[eventTarget.parentElement.cells.length-1].focus();
+    } else {
+      // this.setValue.emit(eventTarget.parentElement.lastElementChild.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex-1].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex-1].focus();
+    }
+
+  }
+
+  @HostListener('keydown.arrowright', ['$event.target']) arrowRight(
+    event: EventTarget
+  ):void {
+    let eventTarget = event as any;
+    eventTarget.classList.remove('selected');
+    if (eventTarget.cellIndex === eventTarget.parentElement.cells.length-1) {
+      // this.setValue.emit(eventTarget.previousSibling.value);
+      eventTarget.cellIndex[0].classList.add('selected');
+      eventTarget.cellIndex[0].focus();
+    } else {
+      // this.setValue.emit(eventTarget.parentElement.lastElementChild.value);
+      eventTarget.parentElement.cells[eventTarget.cellIndex+1].classList.add('selected');
+      eventTarget.parentElement.cells[eventTarget.cellIndex+1].focus();
+    }
+  }
+
+  @HostListener('click', ['$event.target']) addFocus(event: EventTarget):void {
+    let eventTarget = event as any;
+console.log('tghsrthwshswh');
+
+    Array.from(eventTarget.parentElement.cells).map((el:any)=>el.classList.remove('selected'));
+    eventTarget.classList.add('selected');
+    eventTarget.focus();
+
+   
+  }
+
 
 }
