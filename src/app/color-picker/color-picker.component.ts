13a60c668a97ad98f1d9e82235230a0e1f9f94e4
vlmaslennikov <v.maslennikov.q@gmail.com>
2021-06-16 12:29:44 +0300

    add  layout for template and components

--- src/app/color-picker/color-picker.component.ts
+++ src/app/color-picker/color-picker.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
   styleUrls: ['./color-picker.component.scss']
 })
-export class ColorPickerComponent implements OnInit {
+export class ColorPickerComponent   {
+  palette:string[]=[
+    'red','green','blue','black','grey','orange','yellow','pink','white','violet'
+  ];
+  selectedColor!:string;
+  currentColor!:string;
+  
+  @ViewChild('paletteDropdown') paletteDropdown!:ElementRef
 
-  constructor() { }
 
-  ngOnInit(): void {
+  selectColor(){
+
   }
-
+ 
 }
