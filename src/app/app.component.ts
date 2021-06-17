d5cc2fa45b6136151111c23767c6b1d0cddbe531
vlmaslennikov <v.maslennikov.q@gmail.com>
2021-06-17 17:03:24 +0300

    change forms to reactive forms with custom  validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup , AbstractControl, ValidatorFn, Validators} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  colors:string[] = [
+   colors:string[] = [
     'red','green','blue','black','grey','orange','yellow','pink','white','violet','indigo','brown'
   ]
   selectedColor:string ='red';
+  form!: FormGroup;
+  constructor(
+    private readonly formBuilder: FormBuilder,
+  ){
 
+  }
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      color:['',[ Validators.required, this.colorValidator()]]
+    });
+  }
   insertColor(value:string){
      this.selectedColor=value;
    }
-
   
-  
+  colorValidator(): ValidatorFn{
+     return (
+         control: AbstractControl
+      ): {[key: string]: boolean } | null => {
+        let colorRgEx: RegExp = /^[0-9]{20}(?!.)/ ;
+        let valid = !control.value || colorRgEx.test(control.value);
+        return valid ? null : { color: true }
+      }
+    }
 
 }
 
